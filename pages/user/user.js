eee523fb98ad8531d7543f62b705436496ae64f7
fc <327619852@qq.com>
2019-04-09 22:57:04 +0800

    app

--- pages/user/user.js
+++ pages/user/user.js
@@ -1,18 +1,23 @@
 // pages/user/user.js
+const app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    userInfo:{},
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    if(app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo
+      })
+    }
   },
 
   /**
