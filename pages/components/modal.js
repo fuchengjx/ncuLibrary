1724af9af5725bf986c2f8b43a0bf8709c48d530
fc <327619852@qq.com>
2019-04-17 16:19:09 +0800

    modal event

--- pages/components/modal.js
+++ pages/components/modal.js
@@ -19,23 +19,26 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    item: [
+      { name: 'USA', value: '美国', checked:false},
+      { name: 'CHN', value: '美国', checked: true },
+      { name: 'BRA', value: '巴西', checked: false},
+      { name: 'JPN', value: '日本', checked: false},
+      { name: 'ENG', value: '英国', checked: false},
+      { name: 'FRA', value: '法国', checked: false},
+    ],
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-    clickMask() {
+    clickMask(e) {
       this.setData({show: false})
-      console.log("this is clickMask")
+      console.log("this is clickMask",e)
     },
-    cancel() {
-      this.setData({ show: false })
-      this.triggerEvent('cancel')
-    },
-
-    confirm() {
+    confirm(e) {
+      console.log("this is a confirm",e)
       this.setData({ show: false })
       this.triggerEvent('confirm')
     }
